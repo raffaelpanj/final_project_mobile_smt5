5f238589a10574c1919419ca84bafdd1e1f69f85
raffaelpanj <82804034+raffaelpanj@users.noreply.github.com>
2023-01-07 08:45:59 +0700

    Add files via upload

---
+++ Nicelifeapp/app/build/generated/source/buildConfig/debug/com/example/nicelife/apksehat/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.nicelife.apksehat;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.nicelife.apksehat";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from build type: debug
+  public static final String NEWS_API_TOKEN = "61af9d5b707f4bd39537b7652e0f4d9b";
+}
